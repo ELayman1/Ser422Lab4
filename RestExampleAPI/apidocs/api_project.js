cacc29276d963c7b6dcb127557e4b7044ad5c7e2
Elizabeth Layman <elayman1@asu.edu>
2019-04-04 12:17:38 -0700

    initial commit

---
+++ RestExampleAPI/apidocs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "BooktownREST-example",
+  "version": "0.1.0",
+  "description": "SER422 REST example project",
+  "title": "Booktown REST API",
+  "url": "https://localhost:8080/RestExampleAPI/rest/",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-04-04T19:15:53.213Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
